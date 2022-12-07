dbec85835c4560bfbedd85a3085fbdf7d051d3e9
Oscar Horta <65254258+Oscarhendrix666@users.noreply.github.com>
2022-12-07 20:18:27 -0300

    Update sql_practice_07-12-22.sql

--- sql_practice_07-12-22.sql
+++ sql_practice_07-12-22.sql
@@ -16,3 +16,18 @@ select * from sakila.staff where staff_id <>2;  #<> diferente
 select * from sakila.language where name<>'German';
 select title, description, release_year from sakila.film where title ='IMPACT ALADDIN';
 select * from sakila.payment where amount>0.99 order by amount ASC;
+select * from sakila.country where country = 'Algeria';
+select * from sakila.language where language_id = 2 or name = 'German';
+select * from sakila.category where not name = 'Action';
+select distinct(rating) from sakila.film where not rating = 'PG';
+
+#and es utilizado para dos condiciones verdaderas
+select * from sakila.payment
+where customer_id = 36
+and amount>0.99
+and staff_id = 1;
+
+select * from sakila.rental
+where not staff_id = 1
+and customer_id > 250
+and inventory_id < 100;
